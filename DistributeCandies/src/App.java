5ec4a4d58b1908be5f8883c09574908d7c917db4
Ben Adu <benedictadu@gmail.com>
2021-10-05 21:45:42 -0400

    2nd run through

--- DistributeCandies/src/App.java
+++ DistributeCandies/src/App.java
@@ -3,9 +3,9 @@ public class App {
    public static void main(String[] args) {
        Solution solution = new Solution();
 
-       int [] candyType = {1,1,2,2,3,3};
+       int [] candyType = {6,6,6,6,6};
        int answer = solution.distributeCandies(candyType);
        
-       System.out.println(answer);
+      System.out.println(answer);
    }
 }
--- DistributeCandies/src/Solution.java
+++ DistributeCandies/src/Solution.java
@@ -1,23 +1,11 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 class Solution {
     public int distributeCandies(int[] candyType)
     {
-        int uniqueCandies =0;
-
-        for(int i = 0; i< candyType.length; i++){
-            
-            boolean isUnique =true;
-            for (int j=0; j<i; j++){
-                if (candyType[i]==candyType[j]){
-                    isUnique=false;
-                    break;
-                }
-            }
-            if (isUnique){
-                uniqueCandies ++;
-            }
-
-        }
-        return Math.min(uniqueCandies, candyType.length/2);
-        
+        int[] unique=IntStream.of(candyType).distinct().toArray();
+         //System.out.println(Arrays.toString(unique));
+        return unique.length;
     }
 }
