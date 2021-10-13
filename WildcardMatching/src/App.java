2eb745dfbb2cf37725fcdc6479afbfe546a0fa06
Ben Adu <benedictadu@gmail.com>
2021-10-12 23:27:37 -0400

    WIP

---
+++ WildcardMatching/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Solution solution = new Solution();
+
+        boolean test = solution.isMatch("a", "aa");
+
+        System.out.println(test);
+    }
+}
